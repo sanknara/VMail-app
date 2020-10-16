146fcbc41a33d50f3010f76fafa5156742688a78
Sankara Narayanan <sankarbeeee2016@gmail.com>
2020-10-16 19:04:33 +0530

    update server js

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');
 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
 const app = express();
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
