632e51bdc692e3c9df4ad9eac57d763c6b1ee858
Sankara Narayanan <sankarbeeee2016@gmail.com>
2020-10-16 19:44:50 +0530

    revert changes

--- server.js
+++ server.js
@@ -3,23 +3,6 @@ const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');
 
-const { exec } = require('child_process');
-
-// Set port (default: 3000). For Heroku, we need to use
-// the port set by the environment variable $PORT
-const port = process.env.PORT || 3000;
-
-const command = `json-server --watch db.json --port ${port}`;
-
-exec(command, (err, stdout, stderr) => {
-    if (err) {
-        console.log('Error running exec', err);
-        return;
-    }
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-});
-
 const app = express();
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
